e81b25e860acc34340cdd915223ab56fb97c641b
Amisha Thakur <pampexam@gmail.com>
2025-09-01 22:43:10 +0530

    Fix blank screen issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
-module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss'),
-    require('autoprefixer'),
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
+
